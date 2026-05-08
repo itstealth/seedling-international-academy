79db89f2a734caaf159120ed368d47701cbc1c1b
DanishCodebase <ammarsaifi5376@gmail.com>
2026-05-08 18:34:30 +0530

    refactor: comment out Cambridge faculty and update image paths for Early Years and Science faculty sections

--- app/faculty/page.tsx
+++ app/faculty/page.tsx
@@ -46,10 +46,10 @@ function Tag({ children, color = "navy" }: { children: React.ReactNode, color?: 
 
 const faculty = [
   { name: "CBSE Faculty", subject: "Seedling Public School", img: "/assets/img/feature-3.jpg" },
-  { name: "Cambridge Faculty", subject: "Seedling International Academy", img: "/assets/img/sia-faculty.jpg" },
+  // { name: "Cambridge Faculty", subject: "Seedling International Academy", img: "/assets/img/sia-faculty.jpg" },
   { name: "Sports & Co-Curricular", subject: "All Campuses", img: "/assets/img/smhs-sports.jpg" },
-  { name: "Early Years Educators", subject: "Seedling Wonderland Kids League", img: "/assets/img/feature-6.jpg" },
-  { name: "Science & Labs", subject: "Practical Learning Wing", img: "/assets/img/feature-2.jpg" },
+  { name: "Early Years Educators", subject: "Seedling Wonderland Kids League", img: "/assets/Home/classroom1.webp" },
+  { name: "Science & Labs", subject: "Practical Learning Wing", img: "/assets/img/feature-6.jpg" },
   { name: "Admissions & Counselling", subject: "All Schools", img: "/assets/img/counselling-1.jpg" },
 ];
 
